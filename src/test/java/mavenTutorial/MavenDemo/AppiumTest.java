bc00b828d58d9477ad5320256546b2ddb4625cbf
poonam <poonamkudale29@gmail.com>
2021-08-17 12:52:24 +0530

    first commit

---
+++ src/test/java/mavenTutorial/MavenDemo/AppiumTest.java
@@ -0,0 +1,18 @@
+package mavenTutorial.MavenDemo;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	@Test
+	public void Nativeapp()
+	{
+		System.out.println("Nativeapp");
+	}
+	@Test
+public void MobileApp()
+{
+	System.out.println("Mobile App");
+	
+}
+}
---
+++ src/test/java/mavenTutorial/MavenDemo/SeleniumTest.java
@@ -0,0 +1,19 @@
+package mavenTutorial.MavenDemo;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	@Test
+	public void BrowserAUTomation()
+	{
+		System.out.println("Browseing automation");
+	}
+	
+	@Test
+	public void elementUI()
+	{
+		System.out.println("Elements in UI");
+	}
+
+}
